27cdeb9213ef846e3ceaedd745c32a8e3d263196
alfe <alfe10251@gmail.com>
2019-06-23 10:43:38 +0900

    change layout

--- extension/js.js
+++ extension/js.js
@@ -1,86 +1,109 @@
-const vrcFriend = () => {
-    $.get("https://www.vrchat.net/api/1/auth/user/friends?offline=false").then( e =>{
-        $('#reachable-friend').html('');
-        // for grouping
-        const public_worlds = {};
-        const private_worlds = {};
-        e.map(user => {
-            if (user.location === "offline" || user.location === "private") return;
-            if (!user.location.includes('~')) {
-                const wrld_hash = user.location;
-                (wrld_hash in public_worlds)
-                    ? public_worlds[wrld_hash].push(user)
-                    : public_worlds[wrld_hash] = [user];
-            }
-            if (user.location.includes('~')) {
-                const wrld_hash = user.location.split("~")[0];
-                (wrld_hash in private_worlds)
-                    ? private_worlds[wrld_hash].push(user)
-                    : private_worlds[wrld_hash] = [user];
-            }
-        })
-        const compare = (a,b) => (a.length < b.length) ? 1 : -1;
-        const public_worldsArr = Object.values(public_worlds).sort(compare);
-        const private_worldsArr = Object.values(private_worlds).sort(compare);
+var rowUserInfos = [];
 
-        // for appending
-        const appendGroups = (worldArr, type) => {
-            $('#reachable-friend').append(`<h3>${type}</h3>`);
-            worldArr.forEach((users, i) => {
-                const worldId = users[0].location.split("~")[0];
-                $('#reachable-friend').append(`
+/* Render Group */
+const appendGroups = (worldArr) => {
+    worldArr.forEach((users, i) => {
+        const worldId = users[0].location.split("~")[0];
+        const type = !users[0].location.includes('~') ? 'Public' : 'Friend';
+        $('#reachable-friend').append(`
+            <div class="flex world-block">
+                <div class="world-type world-type-${type}">${type}</div>
+                <div id="wrd_${type}_${i}" class="world-group"></div>
+                <div class="world-info">
+                    <img id="wrd_${type}_${i}_img">
                     <div class="flex">
-                        <a id="wrd_${type}_${i}" class="world-group"
-                            href="vrchat://launch?ref=vrchat.com&id=${users[0].location}">
-                        </a>
-                        <div class="world-info">
-                            <img id="wrd_${type}_${i}_img">
-                            <a id="wrd_${type}_${i}_name" href="https://www.vrchat.net/home/world/${worldId.split(":")[0]}">${worldId.split(":")[0]}</a>
-                        </div>
+                        <a id="wrd_${type}_${i}_join" href="vrchat://launch?ref=vrchat.com&id=${users[0].location}">JOIN</a>
+                        <a id="wrd_${type}_${i}_name" href="/home/world/${worldId.split(":")[0]}">${worldId.split(":")[0]}</a>
                     </div>
-                `);
-                // world name/img replace
-                $.get(`https://www.vrchat.net/api/1/worlds/${worldId.split(":")[0]}`).then( e => {
-                    $(`#wrd_${type}_${i}_img`).attr('src', e.thumbnailImageUrl);
-                    $(`#wrd_${type}_${i}_name`).text(e.name);
-                });
+                </div>
+            </div>
+        `);
+        // world name/img replace
+        $.get(`/api/1/worlds/${worldId.split(":")[0]}`).then( wrdInfo => {
+            $(`#wrd_${type}_${i}_img`).attr('src', wrdInfo.thumbnailImageUrl);
+            $(`#wrd_${type}_${i}_name`).text(wrdInfo.name);
+            const instances = wrdInfo.instances.filter(instanceInfo => instanceInfo[0].split("~")[0] === worldId.split(":")[1]);
+            if (instances.length !== 0) {
+                $(`#wrd_${type}_${i}_join`).text(`JOIN (${instances[0][1]}人)`);
+            }
+        });
 
-                // show user
-                users.forEach(user => {
-                    $(`#wrd_${type}_${i}`).append(`<div>
-	                    <img width="150" src=${user.currentAvatarThumbnailImageUrl}>
-	                    <span class="user-name">${user.displayName}
-		                    ${user.status === 'active' ? `<span style="color: green">●</span>`
-		                    : user.status === 'busy' ? `<span style="color: red">●</span>`
-		                    : user.status === 'join me' ? `<span style="color: blue">●</span>`
-		                    : `<span></span>`}
-		                    ${user.statusDescription.length === 0 ? '' 
-		                	: `<br/><span class="user-status-description">${user.statusDescription}</span>`}
-	                    </span>
-	                </div>`);
-                });
-            });
+        // show user
+        users.forEach(user => {
+            $(`#wrd_${type}_${i}`).append(`<div>
+                <img width="150" src=${user.currentAvatarThumbnailImageUrl}>
+                <span class="user-name">${user.displayName}
+                    ${user.status === 'active' ? `<span style="color: green">●</span>`
+                    : user.status === 'busy' ? `<span style="color: red">●</span>`
+                    : user.status === 'join me' ? `<span style="color: blue">●</span>`
+                    : `<span></span>`}
+                    ${user.statusDescription.length === 0 ? '' 
+                    : `<br/><span class="user-status-description">${user.statusDescription}</span>`}
+                </span>
+            </div>`);
+        });
+    });
+}
+
+const getWorldArray = (users) => {
+    const worlds = {};
+    users.map(user => {
+        if (user.location === "offline" || user.location === "private") return;
+        if (!user.location.includes('~')) {
+            const wrld_hash = user.location;
+            (wrld_hash in worlds)
+                ? worlds[wrld_hash].push(user)
+                : worlds[wrld_hash] = [user];
         }
-        appendGroups(public_worldsArr, 'Public');
-        appendGroups(private_worldsArr, 'Friend');
+        if (user.location.includes('~')) {
+            const wrld_hash = user.location.split("~")[0];
+            (wrld_hash in worlds)
+                ? worlds[wrld_hash].push(user)
+                : worlds[wrld_hash] = [user];
+        }
     })
+    return worlds;
+}
+const renderReachableFriend = () => {
+    if (rowUserInfos.length === 0) { return };
+    console.info(`${rowUserInfos.length} friends loaded`);
+    const worlds = getWorldArray(rowUserInfos);
+    $('#reachable-friend').html('');
+    appendGroups(Object.values(worlds).sort((a, b) => (a.length < b.length) ? 1 : -1));
+
+};
+
+const vrcFriend = () => {
+    // TODO: use promise
+    rowUserInfos = [];
+    $.get("/api/1/auth/user/friends?offline=false&offset=200").then( e => {
+        rowUserInfos = [...rowUserInfos, ...e];
+        renderReachableFriend();
+    });
+    $.get("/api/1/auth/user/friends?offline=false&offset=100").then( e => {
+        rowUserInfos = [...rowUserInfos, ...e];
+        renderReachableFriend();
+    });
+    $.get("/api/1/auth/user/friends?offline=false").then( e => {
+        rowUserInfos = [...rowUserInfos, ...e];
+        renderReachableFriend();
+    });
 };
 
 const init = () => {
-    console.info('start friends loading');
-    $('.home-content .center-block').append('<button id="reload-friend">Reload</button>').append('<div id="reachable-friend"></div>');
+    $('.home-content .center-block')
+    	.append('<button id="reload-friend"><span class="fas fa-sync"></span></button>')
+    	.append('<div id="reachable-friend"></div>');
 
     if ($('#reachable-friend').length === 0) {
         setTimeout(init, 3000);
         console.info('cannot make reload button');
         return;
     }
-    
+
+    console.info('start friends loading');
     vrcFriend();
-    $('#reload-friend').on('click', () => {
-        vrcFriend();
-    });
-    console.info('end friends loading');
+    $('#reload-friend').on('click', vrcFriend);
 }
 
 window.onload = () => {
